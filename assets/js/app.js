fd017a1a83f62107a2e21d52ca31bea0218bdf45
Emerson Espinoza <xinayespinoza@gmail.com>
2023-04-10 00:22:14 -0400

    estructura

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-import datos from "../data/data.json" assert { type: "json" };
+import datos from "../../data/data.json" assert { type: "json" };
 import { Gift } from "./clases.js";
 
 const cuerpoTabla = document.querySelector("#cuerpo-tabla");
